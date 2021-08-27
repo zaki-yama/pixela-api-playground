1eb3cd1721f6e190ab3b218e17201d095268857e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-08-27 22:38:00 +0900

    feat: use react-hook-form (post-graph)

--- pages/graphs/post-graph.tsx
+++ pages/graphs/post-graph.tsx
@@ -7,17 +7,19 @@ import {
   Select,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Alert,
   AlertIcon,
   Code,
   Stack,
   chakra,
 } from "@chakra-ui/react";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 import Layout from "../../components/layout";
 
-type Form = {
+type FormData = {
   username: string;
   token: string;
 
@@ -63,41 +65,35 @@ const postGraph = async (
 };
 
 export default function PostGraph() {
-  const [form, setForm] = useState<Form>({
-    username: "",
-    token: "",
-    id: "",
-    name: "",
-    unit: "",
-    type: "int",
-    color: "shibafu",
+  const {
+    register,
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    defaultValues: {
+      username: "",
+      token: "",
+      id: "",
+      name: "",
+      unit: "",
+      type: "int",
+      color: "shibafu",
+    },
   });
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [response, setResponse] = useState(null);
   const [error, setError] = useState<any>(null);
 
-  const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-    key: keyof Form
-  ) => {
-    setForm({ ...form, [key]: event.target.value });
-  };
-
-  const handleRadioChange = (value: String, key: keyof Form) => {
-    setForm({ ...form, [key]: value });
-  };
-
-  const handleExecute = async () => {
-    setIsLoading(true);
-    const { username, token, ...body } = form;
+  const onSubmit = async () => {
+    const { username, token, ...body } = getValues();
     try {
       const response = await postGraph(username, token, body);
       console.log(response);
       setResponse(response);
+      setError(null);
     } catch (error) {
       setError(error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
@@ -112,95 +108,129 @@ export default function PostGraph() {
       <Heading size="md" py="4">
         Request parameters
       </Heading>
-      <Stack spacing={4}>
-        <FormControl>
-          <FormLabel>username</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => handleChange(e, "username")}
-            value={form.username}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>token</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => handleChange(e, "token")}
-            value={form.token}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>id</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => handleChange(e, "id")}
-            value={form.id}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>name</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => handleChange(e, "name")}
-            value={form.name}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>unit</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => handleChange(e, "unit")}
-            value={form.unit}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>type</FormLabel>
-          <RadioGroup
-            onChange={(value) => handleRadioChange(value, "type")}
-            value={form.type}
-          >
-            <Stack spacing={4} direction="row">
-              <Radio value="int">int</Radio>
-              <Radio value="float">float</Radio>
-            </Stack>
-          </RadioGroup>
-        </FormControl>
-        <FormControl>
-          <FormLabel>color</FormLabel>
-          <Select onChange={(e) => handleChange(e, "color")} value={form.color}>
-            <option value="shibafu">shibafu (green)</option>
-            <option value="momiji">momiji (red)</option>
-            <option value="sora">sora (blue)</option>
-            <option value="ichou">ichou (yellow)</option>
-            <option value="ajisai">ajisai (purple)</option>
-            <option value="kuro">kuro (black)</option>
-          </Select>
-        </FormControl>
-        <Button
-          type="button"
-          colorScheme="teal"
-          isLoading={isLoading}
-          onClick={handleExecute}
-        >
-          Execute
-        </Button>
-        {response && (
-          <Code display="block" whiteSpace="pre">
-            {JSON.stringify(response, null, 2)}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={4}>
+          <FormControl isInvalid={!!errors.username}>
+            <FormLabel htmlFor="username">username</FormLabel>
+            <Input
+              id="username"
+              type="text"
+              {...register("username", {
+                required: "This is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.username && errors.username.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.token}>
+            <FormLabel htmlFor="token">token</FormLabel>
+            <Input
+              id="token"
+              type="text"
+              {...register("token", {
+                required: "This is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.token && errors.token.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.id}>
+            <FormLabel htmlFor="id">id</FormLabel>
+            <Input
+              id="id"
+              type="text"
+              {...register("id", {
+                required: "This is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.id && errors.id.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="name">name</FormLabel>
+            <Input
+              id="name"
+              type="text"
+              {...register("name", {
+                required: "This is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.unit}>
+            <FormLabel htmlFor="unit">unit</FormLabel>
+            <Input
+              id="unit"
+              type="text"
+              {...register("unit", {
+                required: "This is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.unit && errors.unit.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel>type</FormLabel>
+            <Controller
+              control={control}
+              name="type"
+              render={({ field: { onChange, value } }) => {
+                return (
+                  <RadioGroup onChange={onChange} value={value}>
+                    <Stack spacing={4} direction="row">
+                      <Radio value="int">int</Radio>
+                      <Radio value="float">float</Radio>
+                    </Stack>
+                  </RadioGroup>
+                );
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>color</FormLabel>
+            <Select
+              {...register("color", {
+                required: "This is required",
+              })}
+            >
+              <option value="shibafu">shibafu (green)</option>
+              <option value="momiji">momiji (red)</option>
+              <option value="sora">sora (blue)</option>
+              <option value="ichou">ichou (yellow)</option>
+              <option value="ajisai">ajisai (purple)</option>
+              <option value="kuro">kuro (black)</option>
+            </Select>
+          </FormControl>
+          <Button type="submit" colorScheme="teal" isLoading={isSubmitting}>
+            Execute
+          </Button>
+        </Stack>
+      </form>
+      <Heading size="md" pt="12" pb="4">
+        Response
+      </Heading>
+      {response && (
+        <Code display="block" whiteSpace="pre">
+          {JSON.stringify(response, null, 2)}
+        </Code>
+      )}
+      {error && (
+        <>
+          <Alert status="error">
+            <AlertIcon />
+            {error.status}
+          </Alert>
+          <Code colorScheme="red" display="block" whiteSpace="pre">
+            {JSON.stringify(error.info, null, 2)}
           </Code>
-        )}
-        {error && (
-          <>
-            <Alert status="error">
-              <AlertIcon />
-              {error.status}
-            </Alert>
-            <Code colorScheme="red" display="block" whiteSpace="pre">
-              {JSON.stringify(error.info, null, 2)}
-            </Code>
-          </>
-        )}
-      </Stack>
+        </>
+      )}
     </Layout>
   );
 }
