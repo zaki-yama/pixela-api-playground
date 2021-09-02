a13bca80b97643ffce8a90989ef273740bb2c822
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-09-02 22:52:00 +0900

    feat: implement POST /graphs/<graphID>

---
+++ pages/pixel/post-pixel.tsx
@@ -0,0 +1,150 @@
+import {
+  Heading,
+  Button,
+  Input as ChakraUIInput,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Alert,
+  AlertIcon,
+  Code,
+  Stack,
+  chakra,
+} from "@chakra-ui/react";
+import Input from "../../components/forms/input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import Layout from "../../components/layout";
+
+type FormData = {
+  username: string;
+  token: string;
+  graphId: string;
+  date: string;
+  quantity: string;
+  optionalData?: string;
+};
+
+const postPixel = async (
+  username: string,
+  token: string,
+  graphId: string,
+  body: Record<string, any>
+) => {
+  const res = await fetch(
+    `https://pixe.la/v1/users/${username}/graphs/${graphId}`,
+
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-USER-TOKEN": token,
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  if (!res.ok) {
+    // TODO: define custom error
+    const error = new Error("API request failed");
+    // @ts-ignore
+    error.info = await res.json();
+    // @ts-ignore
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+export default function PostPixel() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    defaultValues: {
+      username: "",
+      token: "",
+      graphId: "",
+      date: "",
+      quantity: "",
+      optionalData: "",
+    },
+  });
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState<any>(null);
+
+  const onSubmit = async () => {
+    const { username, token, graphId, ...body } = getValues();
+    try {
+      const response = await postPixel(username, token, graphId, body);
+      console.log(response);
+      setResponse(response);
+      setError(null);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return (
+    <Layout>
+      <Heading as="h1" size="lg">
+        POST - /v1/users/&lt;username&gt;/graphs/&lt;graphID&gt;
+      </Heading>
+
+      <chakra.p py="4">
+        It records the quantity of the specified date as a &quot;Pixel&quot;.
+      </chakra.p>
+
+      <Heading size="md" py="4">
+        Request parameters
+      </Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={4}>
+          <Input name="username" required register={register} errors={errors} />
+          <Input name="token" required register={register} errors={errors} />
+          <Input name="graphId" required register={register} errors={errors} />
+          <FormControl isInvalid={!!errors.date}>
+            <FormLabel htmlFor="date">date</FormLabel>
+            <ChakraUIInput
+              id="date"
+              type="date"
+              {...register("date", {
+                required: "This is required.",
+                setValueAs: (value) => value.replaceAll("-", ""),
+              })}
+            />
+            <FormErrorMessage>{errors.date?.message}</FormErrorMessage>
+          </FormControl>
+          <Input name="quantity" required register={register} errors={errors} />
+          <Input name="optionalData" register={register} errors={errors} />
+          <Button type="submit" colorScheme="teal" isLoading={isSubmitting}>
+            Execute
+          </Button>
+        </Stack>
+      </form>
+      <Heading size="md" pt="12" pb="4">
+        Response
+      </Heading>
+      {response && (
+        <Code display="block" whiteSpace="pre">
+          {JSON.stringify(response, null, 2)}
+        </Code>
+      )}
+      {error && (
+        <>
+          <Alert status="error">
+            <AlertIcon />
+            {error.status}
+          </Alert>
+          <Code colorScheme="red" display="block" whiteSpace="pre">
+            {JSON.stringify(error.info, null, 2)}
+          </Code>
+        </>
+      )}
+    </Layout>
+  );
+}
